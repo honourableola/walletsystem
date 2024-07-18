d92b5ce0130daf06e6fa82521bc8c7a08976e78a
samuel oyeneye <alliswelloyeneye2016@gmail.com>
2024-07-18 11:57:45 +0100

    Wallet Repository Implmentation

---
+++ Infrastructure/Repositories/WalletRepository.cs
@@ -0,0 +1,39 @@
+﻿using Application.Abstractions.Repositories;
+using Domain.Entities;
+using Infrastructure.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class WalletRepository : IWalletRepository
+    {
+        private readonly ApplicationContext _context;
+        public WalletRepository(ApplicationContext context)
+        {
+            _context = context;
+        }
+        public async Task<Wallet> Create(Wallet wallet)
+        {
+            var wall = await _context.Wallets.AddAsync(wallet);
+            return await wall;
+                
+        }
+
+        public async Task Delete(Wallet wallet)
+        {
+            var walls = await _context.Wallets.Remove(wallet);
+            return await walls;
+
+        }
+
+        public async Task<Wallet> Update(Wallet wallet)
+        {
+            var wallets = await _context.Wallets.Update(wallet);
+            return await wallets;
+        }
+    }
+}
